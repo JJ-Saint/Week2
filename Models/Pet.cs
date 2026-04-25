6f859bdb4b582f4b7d65e23c2fbc42ace01ecd29
JJ-Saint <jqydjdj@gmail.com>
2026-04-24 20:53:37 -0500

    ddd

---
+++ Models/Pet.cs
@@ -0,0 +1,11 @@
+namespace Console.Models.Pet;
+
+public class Pet
+{
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public int Age { get; set; }
+    public string OwnerName { get; set; }
+    public string Symptom { get; set; }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Console.Models.Dueño;
+using Console.Models.Pet;
+
+List<Dueño> dueño = new List<Dueño>{new Dueño{Name = "Daneizer",LastName = "Mosquera Mena",Age = 21,Adress = "Cr14 Downtown",PetName = "Fulfy",PetBreed = "Shitzu"}};
+List<Pet> pet = new List<Pet>{new Pet{Name = "Fulfy",Breed = "Shitzu",Age = 1,OwnerName = "Daneizer",Symptom = "Diarrea"}};
+bool crazyng = true;
+
+string Options = "Add \n"+
+                 "Remove \n"+
+                 "Show \n"+
+                 "Edit \n";
+
+
+while (crazyng)
+{
+    Console.WriteLine(Options);
+
+
+
+
+
+
+
+
+}
