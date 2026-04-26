9eddb8d06120b7ddb3ccb3f7b08b3b519bce6967
Saint <jqydjdj@gmail.com>
2026-04-26 14:39:46 -0500

    Add Dueño class with pet and owner properties

---
+++ Models/Paciente.cs
@@ -0,0 +1,23 @@
+namespace Console.Models.Dueño;
+
+public class Dueño
+
+{
+
+
+    public string Name { get; set; }
+
+
+    public string LastName { get; set; }
+
+
+    public int Age { get; set; }
+
+
+    public string Adress {get; set; }
+
+
+    public string PetName { get; set; }
+
+
+    public string PetBreed { get; set; }
